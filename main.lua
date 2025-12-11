ce0b378a7570c76386aac6a64cf43b97bcb6ed2a
StarChaser69420 <simonscyrus@gmail.com>
2025-12-11 20:00:44 +1300

    Salve Joker added

--- main.lua
+++ main.lua
@@ -58,6 +58,59 @@ SMODS.Atlas{
     end,
         in_pool = function(self,wawa,wawa2)
         --whether or not this card is in the pool, return true if it is, return false if its not
+        return false
+    end
+}
+SMODS.Atlas{
+    key = 'Salve',
+    path = 'Jokers.png',
+    px = 71,
+    py = 95
+}
+
+
+
+    SMODS.Joker{
+    key = 'Salve',
+    loc_txt = {
+        name = 'Salve',
+        text = {
+            'All {C:attention}Aces{} drawn after',
+            'selecting blind become',
+            '{C:dark_edition}Glass Polychrome{}'
+        }
+    },
+    atlas = 'Salve',
+    rarity = 4,
+    cost = 20,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    pos = {x = 1, y = 0},
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play then
+            if context.other_card.base.id == 14 then
+                -- If already polychrome, add glass
+                if context.other_card.edition and context.other_card.edition.polychrome then
+                    context.other_card:set_ability(G.P_CENTERS.m_glass, true)
+                    return {
+                        extra = {message = "Glass!", colour = G.C.DARK_EDITION},
+                        card = context.other_card
+                    }
+                -- If not polychrome yet, make it polychrome
+                elseif not context.other_card.edition then
+                    context.other_card:set_edition({polychrome = true}, true, true)
+                    return {
+                        extra = {message = "Polychrome!", colour = G.C.DARK_EDITION},
+                        card = context.other_card
+                    }
+                end
+            end
+        end
+    end,
+    in_pool = function(self)
         return true
     end
 }
